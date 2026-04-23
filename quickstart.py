490c53616cb8444615ac43bb1f506c80ffc8b31c
mataeo-eh <mataeo.eh@gmail.com>
2026-04-23 18:39:22 -0500

    edited example usage

--- quickstart.py
+++ quickstart.py
@@ -68,7 +68,7 @@ Usage:
     python quickstart.py --output data/quickstart --engineer-features --discretize -dataset
 
     # Testing
-    python SC2-gamestate-extractor/quickstart.py --process-replay-directory replays --output data/quickstart --workers 2 -dataset -s -EDA --download-replays --bots Eris Deimos --num-replays 100
+    python SC2-gamestate-extractor/quickstart.py --process-replay-directory replays --output data/quickstart --workers 2 -dataset -s -EDA --download-replays --num-replays 1000 -ss 22 -l -d
 """
 from absl import flags
 import sys
